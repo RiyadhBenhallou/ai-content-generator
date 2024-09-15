65cac91fc968bc11b073a77016d301df2cebe231
RiyadhBenhallou <riadhriadh2004@gmail.com>
2024-09-15 18:14:54 +0200

    added google oauth provider

--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -1,5 +1,6 @@
 import github from "next-auth/providers/github";
+import google from "next-auth/providers/google";
 
 export const authConfig = {
-  providers: [github],
+  providers: [github, google],
 };
